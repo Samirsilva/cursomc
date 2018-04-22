291ea1120942cd35235274df7aeb69fdbc47fa1e
Samir Santos <samirsilva16@gmail.com>
2018-04-21 21:06:15 -0300

    Pedido, EstadoPagamento e pagamento

--- src/main/java/com/samirsantos/cursomc/CursomcApplication.java
+++ src/main/java/com/samirsantos/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.samirsantos.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.samirsantos.cursomc.domain.Cidade;
 import com.samirsantos.cursomc.domain.Cliente;
 import com.samirsantos.cursomc.domain.Endereco;
 import com.samirsantos.cursomc.domain.Estado;
+import com.samirsantos.cursomc.domain.Pagamento;
+import com.samirsantos.cursomc.domain.PagamentoComBoleto;
+import com.samirsantos.cursomc.domain.PagamentoComCartao;
+import com.samirsantos.cursomc.domain.Pedido;
 import com.samirsantos.cursomc.domain.Produto;
+import com.samirsantos.cursomc.domain.enums.EstadoPagamento;
 import com.samirsantos.cursomc.domain.enums.TipoCliente;
 import com.samirsantos.cursomc.repositories.CategoriaRepository;
 import com.samirsantos.cursomc.repositories.CidadeRepository;
 import com.samirsantos.cursomc.repositories.ClienteRepository;
 import com.samirsantos.cursomc.repositories.EnderecoRepository;
 import com.samirsantos.cursomc.repositories.EstadoRepository;
+import com.samirsantos.cursomc.repositories.PagamentoRepository;
+import com.samirsantos.cursomc.repositories.PedidoRepository;
 import com.samirsantos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -65,7 +77,18 @@ public class CursomcApplication implements CommandLineRunner{
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "41150100", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "41150100", cli1, c2);
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);		
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -84,6 +107,8 @@ public class CursomcApplication implements CommandLineRunner{
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
 		
 		
 		
---
+++ src/main/java/com/samirsantos/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,11 @@
+package com.samirsantos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.samirsantos.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}
