ec39d69220186b4a7b7dd0f557400b04611f4ccb
Samir Santos <samirsilva16@gmail.com>
2018-06-05 01:28:13 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/samirsantos/cursomc/services/CategoriaService.java
+++ src/main/java/com/samirsantos/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.samirsantos.cursomc.domain.Categoria;
@@ -39,13 +42,19 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("não é possível excluir uma categoria que possúi produtos");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+
+		return repo.findAll(pageRequest);
 	}
 }
